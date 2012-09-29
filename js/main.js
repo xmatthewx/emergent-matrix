6ef352b1b920744c653546c6ea308b8000c5dad0
Matthew Willse <mwillse@gmail.com>
2012-09-29 13:35:47 -0400

    storing and loading locally with json.

--- js/main.js
+++ js/main.js
@@ -1,55 +1,51 @@
 
-// init
 $(window).load(function () {
-    $(".alert").alert('close')
-});
+// all JS in $(window).load
 
 
+/***
+ * init itemslist
+ */
+var itemslist = [];
 
-// the itemslist
-var itemslist = new Array(
-    {
-        id:1,
-        content:'cats',
-        mean_x:100,
-        mean_y:100,
-        count:11
-    },
-    {
-        id:2,
-        content:'dogs',
-        mean_x:200,
-        mean_y:100,
-        count:11
-    },
-    {
-        id:3,
-        content:'apples',
-        mean_x:100,
-        mean_y:200,
-        count:11
-    },
-    {
-        id:4,
-        content:'oranges',
-        mean_x:200,
-        mean_y:200,
-        count:11
-    } // no comma at end!!
-);
+if ( !localStorage.getItem('itemslist') ) { 
+    console.log("no stored items"); 
+    initItems();
+}
+else {
+    itemslist = localStorage.getItem('itemslist');
+    itemslist = JSON.parse(itemslist);
+    
+    console.log("loaded items:" + itemslist); 
+}
 
+
+/***
+ * store itemslist
+ */
+ 
+function storelist() {
+
+    theitems = JSON.stringify(itemslist);
+    
+    localStorage.itemslist = theitems;
+    console.log("stored local: " +localStorage.itemslist);
+}
+
+
+// JSON.parse()
+// JSON.stringify()
 
 /***
  * generate html
  */
 $('#thecanvas').html( function(){
-        
-    
+
     // loop thru list
     for(i=0; i<itemslist.length; i++  ){
-        
+    
         item = itemslist[i];
-        
+    
         // write html
         $('#thecanvas').append( 
             '<div id="i_'+ item.id +'" class="btn '+ item.count +'" style="left:'+item.mean_x+'px; top:'+item.mean_y+'px;" >'
@@ -64,10 +60,8 @@ $('#thecanvas').html( function(){
                 +'</section>'
             +'</li>'
         );
-    }
-   
+    } // END for()
 
-   
 });
 
 
@@ -80,20 +74,21 @@ $('#thecanvas div').bind( "dragstart", function(event, ui) {
     // $(this).clone().appendTo('#thecanvas');
     $(this).siblings().css('z-index','0');
     $(this).css('z-index','10');    
-    $(this).children('.details').fadeOut();
+    $(this).children('.details').clearQueue().fadeOut();
 });
 
 $('#thecanvas div').bind( "drag", function(event, ui) {
-    $(this).children('.details').fadeIn();
+    $(this).children('.details').css('border-color','red');
 });
 
 $('#thecanvas div').bind( "dragstop", function(event, ui) {
-    $(this).children('.details').fadeOut();
-    hello(this);
+    $(this).children('.details').fadeIn('slow').delay(2000).fadeOut('slow');
+    updateitem(this);
+    storelist();
 });
 
 
-function hello(elem){
+function updateitem(elem){
     x = $(elem).css('left');
     y = $(elem).css('top');
     console.log('Item '+elem.id+' moved to: '+x+', '+y); 
@@ -110,4 +105,51 @@ $('#thecanvas li a.icon-trash').click( function(){
 });
 
 
+
+
+
+/***
+ * dummy itemlist
+ */
+function initItems(){
+    // add to itemslist
+    addlist = [
+        {
+            id:1,
+            content:'cats',
+            mean_x:100,
+            mean_y:100,
+            count:11
+        },
+        {
+            id:2,
+            content:'dogs',
+            mean_x:200,
+            mean_y:100,
+            count:11
+        },
+        {
+            id:3,
+            content:'apples',
+            mean_x:100,
+            mean_y:200,
+            count:11
+        },
+        {
+            id:4,
+            content:'oranges',
+            mean_x:200,
+            mean_y:200,
+            count:11
+        } // no comma at end!!
+    ];   
+    
+    itemslist = itemslist.concat(addlist);
+    console.log( itemslist[1] );   
+    
+};
+
+
+// END $(window).load
+});
 
