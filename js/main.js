fcc7beea61fba240a6a2fefd0acb685ed5cf4d19
Shankari <muras074@newschool.edu>
2012-09-29 18:42:29 -0400

    Javascript for sending browser info to php using JSON

--- js/main.js
+++ js/main.js
@@ -82,6 +82,7 @@ $('#thecanvas div').bind( "drag", function(event, ui) {
 $('#thecanvas div').bind( "dragstop", function(event, ui) {
     $(this).children('.details').fadeIn('slow').delay(2000).fadeOut('slow');
     updateitem(this);
+    saveIteration(this);
     storelist();
 });
 
@@ -92,6 +93,90 @@ function updateitem(elem){
     console.log('Item '+elem.id+' moved to: '+x+', '+y); 
     }
 
+//---------------------------------------------------------------------------
+/* Send Item's new position to setData.php in JSON
+	Function Definitions:
+	saveIteration(elem): Creates a JSONString using the updated browser information
+	getHTTPObject():
+	runAjax(JSONstring):
+	sendData(): 
+*/
+
+ function saveIteration(elem){
+    x = $(elem).css('left');
+    y = $(elem).css('top');
+    
+    var JSONObject = new Object;
+    
+    JSONObject.ITEM_ID=elem.id.slice(2);
+    JSONObject.ITEM_X=x.slice(0,-2);
+    JSONObject.ITEM_Y=y.slice(0,-2);
+    
+    JSONstring = JSON.stringify(JSONObject);
+    runAjax(JSONstring);
+    
+    console.log(''+JSONstring);
+  }
+
+function getHTTPObject()
+{
+    var xhr = false;
+    if (window.XMLHttpRequest)
+    {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        try
+        {
+            xhr = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch(e)
+        {
+            try
+            {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch(e)
+            {
+                xhr = false;
+            }
+        }
+    }
+    return xhr;
+}
+
+var request;
+function runAjax(JSONstring)
+{
+    // function returns "AJAX" object, depending on web browser
+    // this is not native JS function!
+    request = getHTTPObject();
+    request.onreadystatechange = sendData;
+    request.open("GET", "php/setData.php?json="+JSONstring, true);
+    request.send(null);
+}
+
+
+
+// function is executed when var request state changes
+function sendData()
+{
+    // if request object received response
+    if(request.readyState == 4)
+    {
+	// parser.php response
+	var JSONtext = request.responseText;
+	// convert received string to JavaScript object
+	var JSONobject = JSON.parse(JSONtext);
+ 
+	// notice how variables are used
+	var msg = "Number of errors: "+JSONobject.errorsNum+
+		"\n- "+JSONobject.error[0]+
+		"\n- "+JSONobject.error[1];
+
+    }
+}
+
+//---------------------------------------------------------------------------
 
 /***
  * delete
