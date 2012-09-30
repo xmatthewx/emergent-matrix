050458583a3d4cdf74886bb5173c1dfd5817d7a4
Shankari <muras074@newschool.edu>
2012-09-29 20:39:09 -0400

    now saving data into database from external servers

--- php/setData.php
+++ php/setData.php
@@ -2,19 +2,16 @@
 include "config.php";
 
 // decode JSON string to PHP object
-
-$new_iteration = json_decode($_GET['json']);
+$string=str_replace ('\"','"', $_GET['json']);
+$new_iteration = json_decode($string);
 $sql_insert= "insert into ITEM_ITERATIONS(ITEM_ID,ITEM_X,ITEM_Y)
 		 VALUES($new_iteration->ITEM_ID,$new_iteration->ITEM_X,$new_iteration->ITEM_Y)";
 
 mysql_query($sql_insert);
 
-//Validation required
 // create response object
 $json = array();
-$json['errorsNum'] = 2;
-$json['error'] = array();
-$json['error'][] = 'Failure';
+$json['item_information'] = $string;
  
 // encode array $json to JSON string
 $encoded = json_encode($json);
@@ -22,4 +19,5 @@ $encoded = json_encode($json);
 // send response back to index.html
 // and end script execution
 die($encoded);
+
 ?>
