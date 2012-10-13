dd039abaad29e2bf46537d9cfebb706a616645cc
Matthew Willse <mwillse@gmail.com>
2012-10-13 02:29:55 -0400

    removing config

--- php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//    $connection = mysql_connect('emergentmatrix.db.8883474.hostedresource.com', 'emergentmatrix', 'Sparkles101');
-//    mysql_select_db('emergentmatrix', $connection);
-    $connection = mysql_connect('mysql.ideapublic.org', 'matrix2000', 'futurematrix2000');
-    mysql_select_db('ideapublic_matrix', $connection);
-
-//    header('Access-Control-Allow-Origin: *');
