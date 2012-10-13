115fc2172e34c0fad180bee9cf600bc350aa0021
Matthew Willse <mwillse@gmail.com>
2012-10-12 23:49:25 -0400

    finishing newMatrix.php. db write and uri redirect working.

--- php/newMatrix.php
+++ php/newMatrix.php
@@ -12,31 +12,22 @@ include "config.php";
 */
 
 
-// $theuri = $_SERVER['REQUEST_URI'];
-// echo $theuri;
-
-
 $matrixUri = $_POST["MATRIX_URI"];
 $matrixTitle = $_POST["MATRIX_TITLE"];
 $matrixCategories = $_POST["MATRIX_CATS"]; // an array 
 $matrixCategories = implode(",", $matrixCategories); // as string 
 $matrixItems = $_POST["MATRIX_ITEMS"];
+
+//Removing blank items from the array
+$blank_items = array_keys($matrixItems,"");
+foreach ($blank_items as $b)
+unset($matrixItems[$b]);
+
 $matrixItems = implode(",", $matrixItems);
 
-/* 
- * We need to remove blank MATRIX ITEMS
- * (not sure that i can do it on the front end)
- * Something like this:
- 
-    array_filter($_POST['MATRIX_ITEMS'],"remove_empty")
-    function remove_empty($val) {
-        if ($val==="") {
-            // array_splice(array,start,length,array)
-        };
-        return false;
-    };
+//store the form data in an array
+$matrixData=array("matrixUri"=>$matrixUri,"matrixCategories"=>$matrixCategories,"matrixItems"=>$matrixItems,"matrixTitle"=>$matrixTitle);
 
-*/
 
 
 // avoid spam!!
@@ -44,23 +35,42 @@ if ( $_POST["TEST2"] ) { // kill the operation!
     $spammer = true; 
     } 
 else {
-    insertData();
+    insertData($matrixData);
+	
 }
 
 
 /*
  * inserts a new record into the MATRIX table
  */
-function insertData() {
-    echo 'insert data';
-	$sql_insert= "insert into MATRIX(MATRIX_URI,MATRIX_TITLE)
-	VALUES('$matrixUri','$matrixTitle')";
+function insertData($matrixData) {
+    echo 'insert data 2';
+    
+    
+    $sql_insert= "insert into MATRIX(MATRIX_URI,MATRIX_CATS,MATRIX_ITEMS,MATRIX_TITLE)
+	VALUES('$matrixData[matrixUri]','$matrixData[matrixCategories]','$matrixData[matrixItems]','$matrixData[matrixTitle]')";
+    
 	$result = mysql_query($sql_insert);
     if (!$result) {
         die('Invalid query: ' . mysql_error());
     }
-	mysql_query("COMMIT");
-	
+
+	$result2 = mysql_query("COMMIT");
+	echo $result2;
+	if ($result == 1) { 
+	    echo 'commit worked'; 
+	    header("Location: http://www.ideapublic.org/matrix/?id=".$matrixData[matrixUri]); /* Redirect browser */
+        
+	    
+	    }
+    if ($result2 != 1) {
+        die('Invalid commit: ' . mysql_error());
+    }
+
+
+	echo 'commit done?';
+    
+    
 	
 }
 
@@ -70,14 +80,13 @@ function insertData() {
 // then we need to make a template that knows what to do with that uri
 
 
-header("Location: http://www.google.com/"); /* Redirect browser */
 
 /* Make sure that code below does not get executed when we redirect. */
-exit;
+// exit;
 
 // send response back to index.html
 // and end script execution
-die($matrixUri);
+// die($matrixUri);
 
 
 /*
