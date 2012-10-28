5ce3482e490075a9934f4a8e1241749da8369f1a
Shankari <muras074@newschool.edu>
2012-10-28 19:25:52 -0400

    changes to getIterations

--- php/getIterations.php
+++ php/getIterations.php
@@ -5,26 +5,25 @@
     }
     include "config.php";
 
-    // grab vars
-    $uri = $_GET['id']; // matrix uri
-    $item = $_GET['item']; // matrix uri
+    // grab ID of the item belonging to a particular matrix
+    $id = $_GET['id']; // matrix uri
 
     // load data
     $data = array();
 
-    //Joint Query of ITEMS AND MATRIX TABLE to fetch all the required,relevant data in one query
-    $result = mysql_query("SELECT ITEMS.ID,ITEMS.ITEM_ID,ITEMS.ITEM_COUNT,MATRIX.MATRIX_ID,MATRIX.MATRIX_URI FROM ITEMS,MATRIX,ITEMS_ITERATIONS WHERE ITEMS_ITERATIONS.ITEM_ID = ITEMS.ITEM_ID AND ITEMS.ITEM_ID = '$item' AND ITEMS.MATRIX_ID=MATRIX.MATRIX_ID AND MATRIX.MATRIX_URI='$uri'");
+    $result=mysql_query("SELECT ID, ITEM_ID, ITEM_X, ITEM_Y, ITEM_MEAN_X, ITEM_MEAN_Y FROM ITEM_ITERATIONS WHERE ID='$id'");
+
     while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
     	$data[]= array(
+    			'ID'=>$row['ID'],
     			'ITEM_ID'=>$row['ITEM_ID'],
     			'ITEM_X'=>$row['ITEM_X'],
     			'ITEM_Y'=>$row['ITEM_Y'],
-    			'ITEM_COUNT'=>$row['ITEM_COUNT'],
-    			'MATRIX_ID' => $row['MATRIX_ID'],
-    			'MATRIX_URI' => $row['MATRIX_URI']
+    			'ITEM_MEAN_X'=>$row['ITEM_MEAN_X'],
+    			'ITEM_MEAN_Y' => $row['ITEM_MEAN_Y']
     	);
     }
     
-        echo json_encode($data); // return ajax
+    echo json_encode($data); // return ajax
 
 ?>
