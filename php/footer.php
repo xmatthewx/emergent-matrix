dd49fb3a0f6d9d8b5c4c578e4f419e331697017a
Matthew Willse <mwillse@gmail.com>
2012-10-28 22:34:09 -0400

    now works from root. ex: .org/matrix/?id=pets

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@
 
     <div id="info" class="">
         <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <p>Thanks. We will display <strong><span class="user"></span>your input</strong> alongside the average from <strong><span class="swarm2"></span>everyone</strong>.</p> 
+        <p>Thanks. We will display <strong><span class="user"></span>your input</strong> along with the average from <strong><span class="swarm2"></span>everyone</strong>.</p> 
     </div>            
 
 </body>
